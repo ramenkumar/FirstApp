62b15290e913a00a226fcb6aa6fcd0dde65e46a8
ramenbhar <ramen.bhar@co-operative.coop>
2014-07-10 00:09:14 +0530

    Change the message in Application.Java file

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("Hi Ramen");
+        return ok("Hi Ramen ! welcome !");
     }
 
 }
